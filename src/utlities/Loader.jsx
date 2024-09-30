9ae02b4bbe60ebef7fa74d606dcd54abe39ee238
sajib689 <jibonraja11@gmail.com>
2024-09-30 20:49:15 +0600

    implement the code on loader components

--- src/utlities/Loader.jsx
+++ src/utlities/Loader.jsx
@@ -2,9 +2,9 @@
 
 const Loader = () => {
     return (
-        <div>
-            
-        </div>
+        <div className="flex items-center justify-center h-screen">
+        <div className="animate-spin rounded-full border-t-2 border-b-2 border-blue-500 h-16 w-16"></div>
+      </div>
     );
 };
 
