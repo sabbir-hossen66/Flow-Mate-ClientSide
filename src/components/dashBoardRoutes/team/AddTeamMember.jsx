bc2036cd5f419c94b355baede4387c0b958f69d3
sajib689 <jibonraja11@gmail.com>
2024-10-01 11:50:35 +0600

    receive the props from the addTeamMember

--- src/components/dashBoardRoutes/team/AddTeamMember.jsx
+++ src/components/dashBoardRoutes/team/AddTeamMember.jsx
@@ -14,7 +14,7 @@ import { Label } from "@/components/ui/label";
 import UseAxiosCommon from "@/hooks/UseAxiosCommon";
 import Swal from "sweetalert2";
 
-export function AddTeamMember({refetch,reset}) {
+export function AddTeamMember({refetch,reset,team}) {
   const {
     register,
     handleSubmit,
