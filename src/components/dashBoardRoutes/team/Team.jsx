0dc3aa080a59252435379aa6c6ed5c56bceb15ec
sajib689 <jibonraja11@gmail.com>
2024-10-03 13:15:26 +0600

    remove bg color

--- src/components/dashBoardRoutes/team/Team.jsx
+++ src/components/dashBoardRoutes/team/Team.jsx
@@ -7,15 +7,15 @@ import { useLoaderData } from "react-router-dom";
 
 const Team = () => {
   const axiosCommon = UseAxiosCommon();
-  const team = useLoaderData()
- 
+  const team = useLoaderData();
+
   const {
     data: teamMember = [],
     isLoading,
     isError,
     error,
     refetch,
-    reset
+    reset,
   } = useQuery({
     queryKey: ["teamMember"],
     queryFn: async () => {
@@ -23,16 +23,13 @@ const Team = () => {
       return data[0]?.members;
     },
   });
-console.log(teamMember)
   if (isLoading) {
-    return <Loader/>;
+    return <Loader />;
   }
 
   if (isError) {
     return <div>Error: {error.message}</div>;
   }
-
-
 
   return (
     <div className="w-full">
@@ -40,14 +37,14 @@ console.log(teamMember)
         <div className="flex flex-col lg:flex-row justify-between gap-x-3">
           <div className="flex items-center">
             <h2 className="text-lg font-medium text-gray-800 dark:text-white">
-          Team {team?.teamName} members
+              Team {team?.teamName} members
             </h2>
             <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400">
-              {teamMember.length} users
+              {teamMember?.length} users
             </span>
           </div>
           <div className="py-5 lg:py-0">
-            <AddTeamMember refetch={refetch} reset={reset} team={team}/>
+            <AddTeamMember refetch={refetch} reset={reset} team={team} />
           </div>
         </div>
 
@@ -56,22 +53,21 @@ console.log(teamMember)
             <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
               <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
                 <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                  <thead className="bg-gray-50 dark:bg-gray-800">
+                  <thead className=" dark:bg-gray-800">
                     <tr>
                       <th className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <div className="flex items-center gap-x-3">
-                      
                           <span>Name</span>
                         </div>
                       </th>
-                     
+
                       <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         Role
                       </th>
                       <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         Email address
                       </th>
-                      
+
                       <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         Active
                       </th>
@@ -83,7 +79,7 @@ console.log(teamMember)
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                  <tbody className=" divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                     {teamMember?.map((member) => (
                       <tr key={member.id}>
                         <td className="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
@@ -95,7 +91,10 @@ console.log(teamMember)
                             <div className="flex items-center gap-x-2">
                               <img
                                 className="object-cover w-10 h-10 rounded-full"
-                                src={member?.photo || "https://via.placeholder.com/150"}
+                                src={
+                                  member?.photo ||
+                                  "https://via.placeholder.com/150"
+                                }
                                 alt={member?.displayName}
                               />
                               <div>
@@ -109,7 +108,7 @@ console.log(teamMember)
                             </div>
                           </div>
                         </td>
-                      
+
                         <td className="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap">
                           {member?.role}
                         </td>
@@ -117,10 +116,28 @@ console.log(teamMember)
                           {member?.email}
                         </td>
                         <td className="px-4 py-4 text-sm whitespace-nowrap">
-                          <div className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${member.isActive ? 'bg-emerald-100/60 dark:bg-gray-800' : 'bg-red-100/60 dark:bg-gray-800'}`}>
-                            <span className={`h-1.5 w-1.5 rounded-full ${member.isActive ? 'bg-emerald-500' : 'bg-red-500'}`}></span>
-                            <h2 className={`text-sm font-normal ${member.isActive ? 'text-emerald-500' : 'text-red-500'}`}>
-                              {member.isActive ? 'Active' : 'Inactive'}
+                          <div
+                            className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${
+                              member.isActive
+                                ? "bg-emerald-100/60 dark:bg-gray-800"
+                                : "bg-red-100/60 dark:bg-gray-800"
+                            }`}
+                          >
+                            <span
+                              className={`h-1.5 w-1.5 rounded-full ${
+                                member.isActive
+                                  ? "bg-emerald-500"
+                                  : "bg-red-500"
+                              }`}
+                            ></span>
+                            <h2
+                              className={`text-sm font-normal ${
+                                member.isActive
+                                  ? "text-emerald-500"
+                                  : "text-red-500"
+                              }`}
+                            >
+                              {member.isActive ? "Active" : "Inactive"}
                             </h2>
                           </div>
                         </td>
@@ -132,6 +149,13 @@ console.log(teamMember)
                             <button className="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none">
                               Delete
                             </button>
+                            <select
+                              className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+                              defaultValue="member"
+                            >
+                              <option value="admin">Admin</option>
+                              <option value="member">Member</option>
+                            </select>
                           </div>
                         </td>
                       </tr>
