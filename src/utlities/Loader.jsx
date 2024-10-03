702d25726e5c9c779144fb21d32b2ef7754688da
sajib689 <jibonraja11@gmail.com>
2024-10-03 12:35:44 +0600

    fix the loading issu now its work in center

--- src/utlities/Loader.jsx
+++ src/utlities/Loader.jsx
@@ -2,9 +2,10 @@
 
 const Loader = () => {
     return (
-        <div className="flex items-center justify-center h-screen">
-        <div className="animate-spin rounded-full border-t-2 border-b-2 border-blue-500 h-16 w-16"></div>
-      </div>
+      <div className="flex items-center justify-center h-screen overflow-hidden">
+      <div className="animate-spin rounded-full border-t-2 border-b-2 border-blue-500 h-16 w-16"></div>
+    </div>
+    
     );
 };
 
