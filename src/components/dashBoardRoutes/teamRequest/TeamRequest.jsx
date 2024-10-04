e8ac2b8d21f4b1e235dca02113424a65e6f0599c
sajib689 <jibonraja11@gmail.com>
2024-10-04 17:09:36 +0600

    [sajib] fetch the team mebers data

--- src/components/dashBoardRoutes/teamRequest/TeamRequest.jsx
+++ src/components/dashBoardRoutes/teamRequest/TeamRequest.jsx
@@ -1,6 +1,21 @@
+import UseAxiosCommon from "@/hooks/UseAxiosCommon";
+import { useQuery } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
 
 
 const TeamRequest = () => {
+    // Fetch teams using react-query
+    const user = useSelector((state) => state.auth.user);
+  const { data = [], isLoading, error, refetch } = useQuery({
+    queryKey: ['teams', user?.email],
+    queryFn: async () => {
+      if (!user?.email) {
+        return [];
+      }
+      const res = await UseAxiosCommon.get(`/create-team?email=${user?.email}`);
+      return res.data;
+    },
+  });
     return (
         <div>
             
