e7594c8bcd13add91a68b3e1dac1552e59384352
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-10-16 00:32:12 +0600

    [prapty] some responsiveness issue fixed

--- src/Shared/footer/Footer.jsx
+++ src/Shared/footer/Footer.jsx
@@ -63,7 +63,7 @@ const Footer = () => {
               Subscribe to our newsletter for team collaboration tips and
               updates.
             </h1>
-            <div className="flex flex-col mx-auto mt-6 space-y-3 md:space-y-0 md:flex-row">
+            <div className="grid grid-cols-1 md:grid-cols-2  mx-auto mt-6 space-y-3 md:space-y-0 ">
               <form onSubmit={handleSubmit(onSubmit)} className="flex flex-row">
                 <input
                   id="email"
@@ -76,13 +76,13 @@ const Footer = () => {
                       message: "Please enter a valid email",
                     },
                   })}
-                  className="px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                  className="md:px-4 px-3 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
                   placeholder="Email Address"
                   aria-label="Email"
                 />
                 <button
                   type="submit"
-                  className="w-full px-6 py-2.5 text-sm font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
+                  className="w-full md:px-6 px-2 py-2.5 md:text-sm text-xs font-medium tracking-wider text-white transition-colors duration-300 transform md:w-auto md:mx-4 focus:outline-none bg-gray-800 rounded-lg hover:bg-gray-700 focus:ring focus:ring-gray-300 focus:ring-opacity-80"
                 >
                   Subscribe
                 </button>
