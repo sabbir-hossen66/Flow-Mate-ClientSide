ed86a8c1abe7f12359146b6cc74e1fa523398de6
NabilaFerdousPrapty <nabilaferdousprapty@gmail.com>
2024-10-16 18:52:08 +0600

    [prapty] errors fixed

--- src/components/dashBoardRoutes/dashBoardPaidUser/DashBoardPaidUser.jsx
+++ src/components/dashBoardRoutes/dashBoardPaidUser/DashBoardPaidUser.jsx
@@ -1,16 +1,19 @@
 import UseAxiosCommon from "@/hooks/UseAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { MdTurnSharpLeft } from "react-icons/md";
 
 const DashBoardSubscriptionUser = () => {
   const axiosCommon = UseAxiosCommon();
   const user = useSelector((state) => state.auth.user);
 
-
   // Fetching paid user data
-  const { data: paid = [], isLoading, isError } = useQuery({
+  const {
+    data: paid = [],
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["paid"],
 
     queryFn: async () => {
@@ -22,7 +25,6 @@ const DashBoardSubscriptionUser = () => {
     },
   });
 
-
   // Handle loading state
   if (isLoading) {
     return <div>Loading paid user data...</div>;
@@ -33,47 +35,30 @@ const DashBoardSubscriptionUser = () => {
     return <div>Error fetching paid user data.</div>;
   }
 
-
   return (
     <div className="p-6">
       {user ? (
-
         <div className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 shadow-xl rounded-2xl overflow-hidden transform hover:scale-110 transition-transform duration-300 w-64">
-
-        <div className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 shadow-xl rounded-2xl overflow-hidden transform hover:scale-110 transition-transform duration-300 w-72">
-
-        
-
-        <Card className="shadow-xl hover:shadow-2xl transition-transform duration-300 transform hover:scale-105">
-                <CardHeader>
-                  <div className="text-start space-x-4">
-  
-
-                    <CardTitle className="text-lg font-semibold pb-7 text-gray-800">Logged-in </CardTitle>
-
-                    <CardTitle className="text-lg font-semibold pb-7 text-gray-800">Paid </CardTitle>
-
-                     <p className="text-5xl font-bold text-black">
-                     {paid.length}
-                  </p>
-                    
-                  </div>
-                </CardHeader>
-                <CardContent className='flex'>
-                <span className='text-blue-500 font-bold text-2xl'> <MdTurnSharpLeft /></span>
-
-                <p className="text-lg font-semibold">Total Logged-in Users</p>
-
-                <p className="text-lg font-semibold">Total paid Users</p>
-
-                </CardContent>
-              </Card>
-           
+          <Card className="shadow-xl hover:shadow-2xl transition-transform duration-300 transform hover:scale-105">
+            <CardHeader>
+              <div className="text-start space-x-4">
+                <CardTitle className="text-lg font-semibold pb-7 text-gray-800">
+                  Logged-in{" "}
+                </CardTitle>
+                <p className="text-5xl font-bold text-black">{paid.length}</p>
+              </div>
+            </CardHeader>
+            <CardContent className="flex">
+              <span className="text-blue-500 font-bold text-2xl">
+                {" "}
+                <MdTurnSharpLeft />
+              </span>
+              <p className="text-lg font-semibold">Total Logged-in Users</p>
+            </CardContent>
+          </Card>
         </div>
       ) : (
-
         <div>No user logged in.</div>
-
       )}
     </div>
   );
--- src/components/dashBoardRoutes/dashBoardSubscriptionUser/DashBoardSubscriptionUser.jsx
+++ src/components/dashBoardRoutes/dashBoardSubscriptionUser/DashBoardSubscriptionUser.jsx
@@ -1,7 +1,7 @@
 import UseAxiosCommon from "@/hooks/UseAxiosCommon";
 import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { MdTurnSharpLeft } from "react-icons/md";
 
 const DashBoardSubscriptionUser = () => {
@@ -9,7 +9,11 @@ const DashBoardSubscriptionUser = () => {
   const user = useSelector((state) => state.auth.user);
 
   // Fetching subscription user data
-  const { data: subscription = [], isLoading, isError } = useQuery({
+  const {
+    data: subscription = [],
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["subscription"],
     queryFn: async () => {
       const res = await axiosCommon.get("/newsletters");
@@ -30,43 +34,29 @@ const DashBoardSubscriptionUser = () => {
   return (
     <div className="p-6">
       {user ? (
-
         <div className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 shadow-xl rounded-2xl overflow-hidden transform hover:scale-110 transition-transform duration-300 w-64">
-
-        <div className="bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 shadow-xl rounded-2xl overflow-hidden transform hover:scale-110 transition-transform duration-300 w-72">
-
-
-
           <Card className="shadow-xl hover:shadow-2xl transition-transform duration-300 transform hover:scale-105">
             <CardHeader>
               <div className="text-start space-x-4">
-
-
-                <CardTitle className="text-lg font-semibold pb-7 text-gray-800">Logged-in </CardTitle>
-
-                <CardTitle className="text-lg font-semibold pb-7 text-gray-800">Subscription </CardTitle>
-
+                <CardTitle className="text-lg font-semibold pb-7 text-gray-800">
+                  Logged-in{" "}
+                </CardTitle>
                 <p className="text-5xl font-bold text-black">
                   {subscription.length}
                 </p>
-
               </div>
             </CardHeader>
-            <CardContent className='flex'>
-              <span className='text-blue-500 font-bold text-2xl'> <MdTurnSharpLeft /></span>
-
+            <CardContent className="flex">
+              <span className="text-blue-500 font-bold text-2xl">
+                {" "}
+                <MdTurnSharpLeft />
+              </span>
               <p className="text-lg font-semibold">Total Logged-in Users</p>
-
-              <p className="text-lg font-semibold">Total subscription user</p>
-
             </CardContent>
           </Card>
-
         </div>
       ) : (
-
         <div>No user logged in.</div>
-
       )}
     </div>
   );
