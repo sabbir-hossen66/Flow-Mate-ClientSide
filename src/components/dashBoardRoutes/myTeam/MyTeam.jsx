82abb8f23977ebdf10ef7569631bb64d2eb1c358
sajib689 <jibonraja11@gmail.com>
2024-10-16 13:09:01 +0600

    update the bg color on the My Team card

--- src/components/dashBoardRoutes/myTeam/MyTeam.jsx
+++ src/components/dashBoardRoutes/myTeam/MyTeam.jsx
@@ -68,7 +68,7 @@ const MyTeam = () => {
       {currentUserTeams.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {currentUserTeams.map((team) => (
-            <div key={team._id} className="bg-gray-200 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 p-6">
+            <div key={team._id} className="bg-white rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 p-6">
               <h3 className="text-2xl font-[500] mb-2">
                 <Link to={`/dashboard/team/${team?.teamName}`} className="hover:underline">
                 {team?.teamName}
