4bb1e2e6beca3fae2e92ada27e63c1bddff02363
codeXfarm <codexfarm24@gmail.com>
2024-10-22 12:04:44 +0600

    [sabbir] test

--- src/pages/aboutPage/AboutPage.jsx
+++ src/pages/aboutPage/AboutPage.jsx
@@ -3,20 +3,22 @@ import AboutScrum from '@/components/aboutscrum/AboutScrum';
 import TeamOrganize from '@/components/organize/TeamOrganize';
 
 const AboutPage = () => {
- 
+
 
   return (
 
-    <div className="bg-gray-50 py-12">   
-      <AboutScrum/>
-      <TeamOrganize/>
+    <div className="bg-gray-50 py-12">
+      <AboutScrum />
+      <TeamOrganize />
       {/* <FlowMateService/> */}
+      <section className='p-10'>
+        <div>Team page show</div>
+      </section>
 
-    
-    
 
-    
-  </div>
+
+
+    </div>
   );
 };
 
